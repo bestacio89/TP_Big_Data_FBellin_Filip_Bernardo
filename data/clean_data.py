f7633afb4bd998ed85f3e20a9316cd205bc7a1f6
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-10-18 13:54:25 +0200

    [Fix] CSV files and cleaner

--- data/clean_data.py
+++ data/clean_data.py
@@ -7,7 +7,6 @@ def nettoyer_fichier(fichier_entree, fichier_sortie):
       fichier_entree: Le chemin d'accès au fichier CSV d'entrée.
       fichier_sortie: Le chemin d'accès au fichier CSV de sortie nettoyé.
     """
-
     # Charger le fichier CSV
     df = pd.read_csv(fichier_entree, delimiter=',', header=0)
 
@@ -19,16 +18,16 @@ def nettoyer_fichier(fichier_entree, fichier_sortie):
     for column in columns_to_clean:
         df[column] = df[column].astype(str).str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
 
-    # Gérer les valeurs manquantes (à adapter selon les besoins de chaque tâche)
-    # Exemple : remplacer les valeurs manquantes de la colonne `timbrecli` par la moyenne
-    df['timbrecli'].fillna(df['timbrecli'].mean(), inplace=True)
+    # Supprimer les lignes contenant des valeurs nulles dans les colonnes spécifiées
+    df.dropna(subset=columns_to_clean, inplace=True)
 
-    # ... (autres traitements pour les valeurs manquantes)
+    # Remplacer les valeurs manquantes de la colonne `timbrecli` par la moyenne
+    df['timbrecli'].fillna(df['timbrecli'].mean(), inplace=True)
 
     # Enregistrer le dataframe nettoyé dans un nouveau fichier CSV
     df.to_csv(fichier_sortie, index=False)
 
 # Nettoyer les deux fichiers CSV
 # Utilisation de chemins relatifs pour accéder aux fichiers CSV
-nettoyer_fichier("./dataw_fro03.csv", "./dataw_fro03_cleaned.csv")  
-nettoyer_fichier("./dataw_fro03_mini_1000.csv", "./dataw_fro03_mini_1000_cleaned.csv")
+nettoyer_fichier("./dataw_fro03/dataw_fro03.csv", "dataw_fro03/dataw_fro03_cleaned.csv")
+nettoyer_fichier("./dataw_fro03_mini_1000/dataw_fro03_mini_1000.csv", "dataw_fro03_mini_1000/dataw_fro03_mini_1000_cleaned.csv")
